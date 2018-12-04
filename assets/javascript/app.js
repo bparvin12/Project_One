4dee82bd09d3ccebd4f6c041881ef37ab62c81c5
Bijan Parvin <bparvin12@gmail.com>
2018-12-04 10:07:13 -0800

    trying to figure out how to display info into results page

--- assets/javascript/app.js
+++ assets/javascript/app.js
@@ -1,6 +1,6 @@
 //need a function to call upon ajax. 
 //this portion needs to call upon google maps, not yelp. 
-function startSearch () {
+function startSearch() {
     var apiKeyGoogle = "AIzaSyDlIhSIHh3DOCgKFekiOXVtnGCzdkGdxlE"
 
     //need to find a way to convert these to long/lat
@@ -16,7 +16,7 @@ function startSearch () {
     $.ajax({
         url: queryURLGeocoding,
         method: 'GET',
-    }).then(function(response1) {
+    }).then(function (response1) {
         console.log(response1);
         console.log(response1.results[0].geometry.location.lng);
         console.log(response1.results[0].geometry.location.lat);
@@ -29,76 +29,80 @@ function startSearch () {
 
         $.ajax({
             url: queryURLGoogleMaps,
-            method: 'GET',       
-        }).then(function(response2) {
+            method: 'GET',
+        }).then(function (response2) {
             console.log(response2);
+
+            for (var i = 0; i < 7; i++) {
+                // main div card that everything goes into
+                var card = $("<div>");
+                card.addClass("card restaurant-card");
+
+                // contains the figue with image inside 
+                var cardImage = $("<div>");
+                cardImage.addClass("card-image");
+
+                var figure = $("<figure>");
+                figure.addClass("image");
+                figure.addClass("is-4by3");
+
+                var img = $("<img>");
+                img.addClass("restImage");
+                img.attr("src", "https://bulma.io/images/placeholders/1280x960.png");
+                img.attr("alt", "Placeholder Image");
+
+
+                // appending first div to card div 
+                figure.append(img);
+                cardImage.append(figure)
+
+                card.append(cardImage)
+
+                //contains second div of card div
+                var cardContent = $("<div>");
+                cardContent.addClass("card-content");
+
+                var restaurantName = $("<div>");
+                restaurantName.addClass("restaurantName");
+
+                var restName = $("<p>");
+                restName.addClass("title is-4 restName");
+
+                var restAddress = $("<p>");
+                restAddress.addClass("subtitle is-6 restAddress");
+
+                restaurantName.append(restName);
+                restaurantName.append(restAddress);
+                cardContent.append(restaurantName);
+
+                var happyHoursDiv = $("<div>");
+                var happyHoursSpan = $("<span>");
+                happyHoursSpan.attr("id", "happyHours");
+
+                happyHoursDiv.append(happyHoursSpan);
+                cardContent.append(happyHoursDiv);
+
+                card.append(cardContent);
+
+                $(".results").append(card);
+                //everything above this appends result card to html 
+            }
         })
     });
 
-   
+
     //radius is set to 8000 meters which is about 5 miles 
     // var queryURLGoogleMaps1 = "https://maps.googleapis.com/maps/api/place/findplacefromtext/json?input=" + lat + "," + lng + "&inputtype=textquery&fields=photos,formatted_address,name,rating,opening_hours,geometry&key=" + apiKeyGoogle;
-   
 
-    
+
+
 }
 
 
 
 
 
-// main div card that everything goes into
-var card = $("<div>");
-card.addClass("card restaurant-card");
 
-// contains the figue with image inside 
-var cardImage = $("<div>");
-cardImage.addClass("card-image");
-
-var figure = $("<figure>");
-figure.addClass("image");
-figure.addClass("is-4by3");
-
-var img = $("<img>");
-img.addClass("restImage");
-img.attr("src", "https://bulma.io/images/placeholders/1280x960.png");
-img.attr("alt", "Placeholder Image");
-
-
-// appending first div to card div 
-figure.append(img);
-cardImage.append(figure)
-
-card.append(cardImage)
-
-//contains second div of card div
-var cardContent = $("<div>");
-cardContent.addClass("card-content");
-
-var restaurantName = $("<div>");
-restaurantName.addClass("restaurantName");
-
-var restName = $("<p>");
-restName.addClass("title is-4 restName");
-
-var restAddress = $("<p>");
-restAddress.addClass("subtitle is-6 restAddress");
-
-restaurantName.append(restName);
-restaurantName.append(restAddress);
-cardContent.append(restaurantName);
-
-var happyHoursDiv = $("<div>");
-var happyHoursSpan = $("<span>");
-happyHoursSpan.attr("id", "happyHours");
-
-happyHoursDiv.append(happyHoursSpan);
-cardContent.append(happyHoursDiv);
-
-card.append(cardContent);
-
-$(".results").append(card);
-//everything above this appends result card to html 
 
 
 //functions to get the modals working.

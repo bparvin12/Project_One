eee0f7b8cb0c9b02d4ef7be77229e7bffc81aba2
Bijan Parvin <bparvin12@gmail.com>
2018-12-06 11:23:19 -0800

    deleted some extra stuff

--- assets/javascript/app.js
+++ assets/javascript/app.js
@@ -630,4 +630,6 @@ $(document).on("click", "#directionsSubmitButton", function () {
     $("#directionsTabContent").append(imageDiv);
 })
 
-
+$(document).ready(function(){
+    var carousels = bulmaCarousel.attach(); // carousels now contains an array of all Carousel instances
+});

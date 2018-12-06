3078f98e75252950fb09be43b5d2f5879da6b4f2
cindy <cindychu619@yahoo.com>
2018-12-06 10:49:36 -0800

    merging again

--- assets/javascript/app.js
+++ assets/javascript/app.js
@@ -270,21 +270,6 @@ $(".closeSignInModal").click(function () {
     }
 
     
-        else
-        {
-        //if sign in fails, clear form so user can retry
-        if (errorMessage == "" && fieldsMissing == "") {
-
-            // Initialize Firebase
-            var config = {
-                apiKey: "AIzaSyAOF_apbWhRflI5RekKNZkrosejZ8FEeWs",
-                authDomain: "project-01-1543881106905.firebaseapp.com",
-                databaseURL: "https://project-01-1543881106905.firebaseio.com",
-                projectId: "project-01-1543881106905",
-                storageBucket: "project-01-1543881106905.appspot.com",
-                messagingSenderId: "307620256786"
-            };
-
         else 
             {
             //if sign in fails, clear form so user can retry
@@ -338,9 +323,7 @@ $(".closeSignInModal").click(function () {
             });
 
             $("#signInModal").toggleClass("is-active");
-        }
-    };
-});
+});    
 
 
 // SEARCH FORM submit
